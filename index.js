99f8c3417abb62b858698aa1c2783a6d4ef38b41
Ben LIU <ben@backpaddock.com.au>
2017-08-02 22:18:32 +1000

    add src

---
+++ index.js
@@ -0,0 +1,5 @@
+import AppStoreButton from './src/AppStoreButton';
+
+module.exports = {
+    AppStoreButton 
+}
---
+++ src/AppStoreButton.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+class AppStoreButton extends React.Component {
+  render() {
+
+    const imgUrl = "http://linkmaker.itunes.apple.com/images/badges/en-us/badge_appstore-lrg.svg";
+    const aStyle = {
+      display: 'inline-block',
+      overflow: 'hidden',
+      background: 'url(' + imgUrl + ') no-repeat',
+      width: this.props.width || 165,
+      height:  this.props.height || 40,
+    };
+    
+    return (
+      <div>
+        <a
+          style = {aStyle}
+          href  = {this.props.url}
+          >
+        </a>
+      </div>
+    )
+  }
+}
+
+
+export default AppStoreButton
